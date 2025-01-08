f2b6c3f57f6891e7b8f6e28a269391a363fe3eba
Dylan <Quantum64Development@gmail.com>
2025-01-07 22:58:08 -0500

    Update

--- NumberTheory/dta11.lean
+++ NumberTheory/dta11.lean
@@ -7,67 +7,116 @@ open Finset
 
 namespace Nat
 
-def order (a n : ℕ) : ℕ :=
-  n - Nat.findGreatest (λ i => a ^ (n - i) ≡ 1 [MOD n]) (n - 1)
 
-@[reducible] def PrimitiveRoot (g n : ℕ) : Prop :=
-   order g n = n - 1
 
-variable {a n g : ℕ} {hg : g.PrimitiveRoot n}
-variable {p : ℕ}
+theorem division_algorithm (a b : ℕ) (h : b > 0) : ∃ q r : ℕ, a = b * q + r ∧ r < b := by
+  use a / b, a % b
+  constructor
+  · rw [Nat.div_add_mod]
+  · exact Nat.mod_lt a h
+
+
+def order (a p : ℕ) : ℕ :=
+  if h : Prime p ∧ 0 < a ∧ ¬p ∣ a then
+    haveI hex : ∃ n : ℕ, n > 0 ∧ a ^ n ≡ 1 [MOD p] := by
+      existsi p - 1
+      unfold ModEq
+      rw [← Int.ofNat_inj]
+      simp
+      constructor
+      . exact Prime.one_lt h.left
+      . refine Int.ModEq.pow_card_sub_one_eq_one h.left ?_
+        rcases Nat.coprime_or_dvd_of_prime h.left a with (hp | hp)
+        . apply IsCoprime.symm
+          exact isCoprime_iff_coprime.mpr hp
+        . simp_all
+    Nat.find hex
+  else 0
+
+lemma order_one_mod_p (a p : ℕ) (ha : 0 < a ∧ ¬p ∣ a) (hp : Prime p) : a ^ a.order p ≡ 1 [MOD p] := by
+  unfold order
+  rw [dif_pos]
+  simp
+  generalize_proofs h
+  exact (Nat.find_spec h).right
+  constructor
+  exacts [hp, ha]
+
+
+lemma order_gt_zero (a p : ℕ) (ha : 0 < a ∧ ¬p ∣ a) (hp : Prime p) : a.order p > 0 := by
+  unfold order
+  rw [dif_pos]
+  simp
+  constructor
+  exacts [hp, ha]
+
+theorem order_div (a n p : ℕ) (hm : a ^ n ≡ 1 [MOD p]) (ha : 0 < a ∧ ¬p ∣ a) (hn : n > 0) (hp : Prime p) : a.order p ∣ n := by
+  let e := a.order p
+  have : e > 0 := by
+    unfold e order
+    rw [dif_pos]
+    simp
+    constructor
+    exacts [hp, ha]
+  obtain ⟨q, r, hqr, hr⟩ := division_algorithm n e this
+  have : a ^ r ≡ 1 [MOD p] := by
+    have :  (a ^ a.order p) ^ q ≡ 1 [MOD p] := by
+      have : 1 ≡ a ^ a.order p  [MOD p] := by simp [Nat.ModEq.symm, order_one_mod_p a p ha hp]
+      have : (a ^ a.order p) ^ q ≡ 1 ^ q  [MOD p] := by gcongr
+      simp_all
+    suffices hs : (a ^ a.order p) ^ q * a ^ r ≡ (a ^ a.order p) ^ q * 1 [MOD p]
+    . refine Nat.ModEq.cancel_left_of_coprime ?_ hs
+      apply Or.resolve_right (Nat.coprime_or_dvd_of_prime hp ((a ^ a.order p) ^ q))
+      by_contra hneg
+      by_cases qzero : 0 < q
+      . have : (a ^ ((a.order p) * q)).primeFactors = a.primeFactors := by
+          apply Nat.primeFactors_pow
+          subst hqr
+          simp_all [e]
+          constructor
+          intro hoz
+          simp_all only [lt_self_iff_false]
+          intro qz
+          linarith
+        have : primeFactors p ⊆ primeFactors a := by
+          rw [← this]
+          apply Nat.primeFactors_mono
+          rw [Nat.pow_mul]
+          exact hneg
+          simp_all
+          intro hx hy
+          simp_all
+        simp_all
+      . simp_all
+    . simp
+      apply Nat.ModEq.symm
+      apply Nat.ModEq.trans this
+      have : (a ^ a.order p) ^ q * a ^ r ≡ (a ^ (a.order p * q + r)) [MOD p] := by
+        rw [← Nat.pow_mul, ← Nat.pow_add]
+      apply Nat.ModEq.symm
+      apply Nat.ModEq.trans this
+      unfold e at hqr
+      rw [← hqr]
+      exact hm
+  suffices : r = 0
+  . simp_all
+  . by_contra rnz
+    have rge : r > 0 := Nat.pos_of_ne_zero rnz
+    have : e ≤ r := by
+      unfold e order
+      rw [dif_pos]
+      apply Nat.find_min'
+      simp_all
+      constructor
+      exacts [hp, ha]
+    linarith
+
+
+
+--@[reducible] def PrimitiveRoot (g n : ℕ) : Prop :=
+--  order g n = n - 1
 
 def index (a n g : ℕ) : ℕ := Nat.findGreatest (λ i => g ^ i ≡ a [MOD n]) (n - 1)
 
 
-theorem order_def (hp : Nat.Prime p) : a ^ (order a p) ≡ 1 [MOD p] := by
-  unfold order
-  by_cases h : findGreatest (fun i => a ^ (p - i) ≡ 1 [MOD p]) (p - 1) = 0
-  . simp_all
-    rw [Nat.findGreatest_eq_zero_iff] at h
-    refine modEq_of_dvd ?_
-    simp_all
-    by_cases h2 : a ^ p = 0
-    . simp_all
-      exfalso
-  . rw [Nat.findGreatest_eq_zero_iff] at h
-    push_neg at h
-    obtain ⟨i, hi, h⟩ := h
-
-
-theorem order_dvd (h : a ^ n ≡ 1 [MOD p]) (hp : PNat.Prime p) (hn : n > 0) (ha : ¬a ∣ p) : order a p ∣ n := by
-  have a_pos : 0 < a := by
-    by_contra hneg
-    have : a = 0 := Nat.eq_zero_of_le_zero (Nat.le_of_not_gt hneg)
-    simp [this] at h
-    rw [Nat.zero_pow, ModEq.comm, Nat.modEq_zero_iff_dvd, Nat.dvd_one] at h
-    simp at h
-    rw [h] at hp
-    simp at hp
-    exact hn
-  --let g := XgcdType.start (a.toPNat a_pos) p
-  --obtain ⟨f, h_f, v, c, d, e, r⟩ := gcd_props (a.toPNat a_pos) p
-  refine remainder ?_
-
-
-theorem order_dvd_prime_minus_one (hp : Nat.Prime p) : order a p ∣ p - 1 := by
-  unfold order PrimitiveRoot at hg
-  rw [Nat.findGreatest_eq_iff.2] at hg
-  exact hg
-
-
-theorem prime_primitive_root_count : #{g ∈ range n | g.PrimitiveRoot n} = p.totient - 1 := by
-  sorry
-
-
-theorem primitive_root_index_pow (hg : g.PrimitiveRoot n) : g ^ index a n g ≡ a [MOD n] := by
-  unfold index
-  unfold PrimitiveRoot order at hg
-  --rw [Nat.findGreatest_eq_iff.2]
-  rw [Nat.findGreatest_eq] at hg
-  sorry
-
-
-
-
---example (p a b : Nat) (h₀ : Nat.Prime p) (h₁ : 0 < a ∧ a < p ∧ 0 < b ∧ b < p) (h2 : a + b ≡ 0 [MOD p]) : index := by
---  exact p.dvd_or_dvd h2
+#eval order 3 7
